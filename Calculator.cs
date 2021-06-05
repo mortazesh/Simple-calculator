503684f22fce85a50ecae2bfbf49c6164fec311b
mortazesh <77435951+mortazesh@users.noreply.github.com>
2021-06-05 08:00:09 -0700

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,32 @@
+﻿using Simple_calculator.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_calculator.Services
+{
+    public class Calculator : ICalculator
+    {
+        public int Division(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+
+        public int Minus(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        public int Multiplication(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ ICalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_calculator.Interface
+{
+    public interface ICalculator
+    {
+        int Sum(int number1,int number2);
+        int Minus(int number1, int number2);
+        int Division(int number1, int number2);
+        int Multiplication(int number1, int number2);
+    }
+}
